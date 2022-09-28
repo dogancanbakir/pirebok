5af9b254cad7204ceeed80a07f4a5450ba820761
Doğan Can Bakır <dogancanbakir@protonmail.com>
2022-09-28 22:26:20 +0000

    add random number encoding transformer

--- tests/test_random_number_encoding_transformer.py
+++ tests/test_random_number_encoding_transformer.py
@@ -1,16 +1,16 @@
 import pytest
 
-from pirebok.transformers.random_case_swapping_transformer import RandomCaseSwappingTransformer
+from pirebok.transformers.random_number_encoding_transformer import RandomNumberEncodingTransformer
 from pirebok.transformers.transformer import Transformer
 
 
 @pytest.fixture
 def transformer() -> Transformer:
-    return RandomCaseSwappingTransformer()
+    return RandomNumberEncodingTransformer()
 
 
 def test_mutation(transformer: Transformer) -> None:
-    payload = "admin' OR 1=1#"
+    payload = "admin' OR 42=42#"
 
     result = transformer.transform(payload)
 
