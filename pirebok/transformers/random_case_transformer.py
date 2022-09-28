ed8bab3d758798c7940b128404e42b73045a3c6f
Doğan Can Bakır <dogancanbakir@protonmail.com>
2022-09-28 14:28:09 +0000

    add random case transformer

---
+++ pirebok/transformers/random_case_transformer.py
@@ -0,0 +1,8 @@
+import random
+
+from pirebok.transformers.transformer import Transformer
+
+
+class RandomCaseTransformer(Transformer):
+    def transform(self, payload: str) -> str:
+        return "".join(map(lambda x: x.swapcase() if random.random() > 0.5 else x, payload))
