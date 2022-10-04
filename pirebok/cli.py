74d3aaf3db3f2efd406e63a0e5f6d86c27977c4b
Doğan Can Bakır <dogancanbakir@protonmail.com>
2022-10-04 09:52:45 +0000

    remove for loop from fuzzers

--- pirebok/cli.py
+++ pirebok/cli.py
@@ -1,4 +1,5 @@
 import click
+from tqdm.auto import trange
 
 from pirebok.fuzzers import Fuzzer, FuzzerBuilder
 
@@ -13,13 +14,12 @@ from pirebok.fuzzers import Fuzzer, FuzzerBuilder
     ),
     help="choose fuzzer",
 )
-@click.option('-e', '--epoch', default=1, help="Number of iteration")
-@click.option('-b', '--batch_size', default=10, help="Total number of payload generation per epoch")
+@click.option('-s', '--steps', default=10, help="Number of iteration")
 @click.option('-p', '--payload', required=True, help="payload to fuzz")
-def main(fuzzer: str, epoch: int, batch_size: int, payload: str) -> None:
+def main(fuzzer: str, steps: int, payload: str) -> None:
     fuzzer_builder = FuzzerBuilder()
     fzzer = fuzzer_builder.choice(fuzzer).build()
-    print('\n'.join(map(str, fzzer.fuzz(payload, epoch, batch_size))))
+    print('\n'.join(map(str, set(map(lambda _: fzzer.fuzz(payload), trange(steps))))))
 
 
 if __name__ == "__main__":
--- pirebok/fuzzers/generic/random_generic_fuzzer.py
+++ pirebok/fuzzers/generic/random_generic_fuzzer.py
@@ -1,20 +1,12 @@
 import random
-from typing import Set
 
 from pirebok.fuzzers.fuzzer_visitor import FuzzerVisitor
 from pirebok.fuzzers.generic_fuzzer import GenericFuzzer
 
 
 class RandomGenericFuzzer(GenericFuzzer):
-    def fuzz(self, payload: str, epoch: int, batch_size: int) -> Set[str]:
-        payloads = set()
-        payload_buff = payload
-        for _ in range(epoch):
-            for _ in range(batch_size):
-                payload_buff = random.choice(self.transformers).transform(payload_buff)
-                payloads.add(payload_buff)
-
-        return payloads
+    def fuzz(self, payload: str) -> str:
+        return random.choice(self.transformers).transform(payload)
 
     def accept(self, visitor: FuzzerVisitor) -> None:
         visitor.visit_generic(self)
